3347e046b524a74b9755b5d8c25d457415efa3b6
k510034208_GamingPC <k510034208.n@gmail.com>
2021-02-26 22:08:08 +0900

    socketio修正

--- co-study-room/socket.js
+++ co-study-room/socket.js
@@ -4,12 +4,16 @@ const db = require('./models/index');
 var sessionMiddleware = app.session;
 
 // express-session を socket.io でも使えるように設定する
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.prototype._init = myIo => {
+  myIo.on('connection', (socket) => {
+    console.log('a user connected');
 
-  // ルームに参加
-  socket.on('join room', roomid => {
-    console.log(`join room, ${ roomid }. userid : ${ socket.request.session.loginuser.id }`);
-    socket.join(roomid);
+    // ルームに参加
+    socket.on('join room', roomid => {
+      console.log(`join room, ${ roomid }. userid : ${ socket.request.session.loginuser.id }`);
+      socket.join(roomid);
+    });
   });
-});
+} 
+
+module.exports = io;
